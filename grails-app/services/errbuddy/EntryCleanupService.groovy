ab7c41e1f32f8dd3d95ba0382544fb54024feefa
Philipp Eschenbach <philipp@uberall.com>
2017-03-27 16:00:23 +0200

    make cleanup search faster

--- grails-app/services/errbuddy/EntryCleanupService.groovy
+++ grails-app/services/errbuddy/EntryCleanupService.groovy
@@ -9,7 +9,7 @@ class EntryCleanupService {
 
 	void handleEntryCleanupForApplication(App app) {
 		int max = 500
-		def entries = Entry.createCriteria().list([max: max, order: 'asc', sort: 'id']) {
+		List oldest = Entry.createCriteria().list([max: max, order: 'asc', sort: 'id']) {
 			entryGroup {
 				eq('app', app)
 			}
@@ -19,14 +19,28 @@ class EntryCleanupService {
 			}
 		} as List
 
-		def toDelete = entries.findAll { it[1].isBefore(app.clearUntil) }.collect { it[0] }.flatten()
-		if (!toDelete) {
-			return
-		}
+		if (oldest && oldest.first().dateCreated.isBefore(app.clearUntil)) {
+			def entries = Entry.createCriteria().list([max: max, order: 'asc', sort: 'id']) {
+				entryGroup {
+					eq('app', app)
+				}
+				projections {
+					property('id')
+					property('dateCreated')
+				}
+			} as List
 
-		log.info "deleting ${toDelete.size()} entries for $app.name"
-		toDelete.each { long entryId ->
-			jesqueService.enqueue(EntryDeleteJob.queueName, EntryDeleteJob, [entryId, true])
+			def toDelete = entries.findAll { it[1].isBefore(app.clearUntil) }.collect { it[0] }.flatten()
+			if (!toDelete) {
+				return
+			}
+
+			log.info "deleting ${toDelete.size()} entries for $app.name"
+			toDelete.each { long entryId ->
+				jesqueService.enqueue(EntryDeleteJob.queueName, EntryDeleteJob, [entryId, true])
+			}
+		} else {
+			log.info "skipping search for outdated errors as oldest is not old enough for $app.name"
 		}
 	}
 }
